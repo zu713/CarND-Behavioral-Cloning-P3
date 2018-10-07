31baebd1d0f4f4eb7c42580c767ff2e358475e44
zu713 <shrn.fz@gmail.com>
2018-10-07 00:11:39 +0000

    changes to model

--- model.py
+++ model.py
@@ -1,34 +1,75 @@
 import csv
 import cv2
 import numpy as np
-from sklearn.model_selection import validation_split
+from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 from keras.models import Sequential 
-from keras.layers import Flatten, Dense, Lambda
-from keras.layers.convolutional import Convolution2D
-from keras.layers.pooling import 
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
 from keras.optimizers import Adam
 import matplotlib
 import matplotlib.pyplot as plt
 
-
+path = 'driving_log.csv'
 lines = []
 
+with open(path) as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+        
+images = []
+measurements = []
+for line in lines:
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = 'IMG/' + filename
+    print(current_path)
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+    
+X_train = np.array(images)
+y_train = np.array(measurements)
+
 model = Sequential()
-model.add(Lambda(lambda: x: x/255.0 - 0.5, input_shape=(160,320,3)))
-#model.add(Flatten(input_shape = (160, 320, 3)))
-#model.add(Dense(1))
-model.add(Cropping2D(cropping=((70,25),(0,0))))
-model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
-model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
-model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
-model.add(Convolution2D(64,3,3,activation="relu"))
-model.add(Convolution2D(64,3,3,activation="relu"))
-model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Dense(1))
+#model.add(Cropping2D(cropping=((70,25),(0,0))))
+#model.add(Conv2D(24,5,5,subsample=(2,2),activation="relu"))
+#model.add(Conv2D(36,5,5,subsample=(2,2),activation="relu"))
+#model.add(Conv2D(48,5,5,subsample=(2,2),activation="relu"))
+#model.add(Conv2D(64,3,3,activation="relu"))
+#model.add(Conv2D(64,3,3,activation="relu"))
+#model.add(Flatten())
+#model.add(Dense(100))
+#model.add(Dense(50))
+#model.add(Dense(10))
 model.compile(loss = 'mse', optimizer = 'adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle = True,nb_epochs)
+model.fit(X_train, y_train, validation_split=0.2, shuffle = True, nb_epoch=5)
 
 model.save('model.h5')
+
+from keras.models import Model
+import matplotlib.pyplot as plt
+
+history_object = model.fit_generator(train_generator, samples_per_epoch =
+    len(train_samples), validation_data = 
+    validation_generator,
+    nb_val_samples = len(validation_samples), 
+    nb_epoch=5, verbose=1)
+
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
