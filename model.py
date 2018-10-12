058f5b0d851df662f677e2643ca4611e572cbacb
zu713 <shrn.fz@gmail.com>
2018-10-12 02:30:30 +0000

    ..model changes

--- model.py
+++ model.py
@@ -8,10 +8,11 @@ from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
 from keras.optimizers import Adam
+from keras.models import Model
 import matplotlib
 import matplotlib.pyplot as plt
 
-path = 'driving_log.csv'
+path = 'Udacity_Data/driving_log.csv'
 lines = []
 
 with open(path) as csvfile:
@@ -24,8 +25,7 @@ measurements = []
 for line in lines:
     source_path = line[0]
     filename = source_path.split('/')[-1]
-    current_path = 'IMG/' + filename
-    print(current_path)
+    current_path = 'Udacity_Data/IMG/' + filename
     image = cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
@@ -36,25 +36,25 @@ y_train = np.array(measurements)
 
 model = Sequential()
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
-model.add(Flatten(input_shape=(160, 320, 3)))
+#model.add(Flatten(input_shape=(160, 320, 3)))
+#model.add(Dense(1))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Conv2D(24,5,5,subsample=(2,2),activation="relu"))
+model.add(Conv2D(36,5,5,subsample=(2,2),activation="relu"))
+model.add(Conv2D(48,5,5,subsample=(2,2),activation="relu"))
+model.add(Conv2D(64,3,3,activation="relu"))
+model.add(Conv2D(64,3,3,activation="relu"))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
-#model.add(Cropping2D(cropping=((70,25),(0,0))))
-#model.add(Conv2D(24,5,5,subsample=(2,2),activation="relu"))
-#model.add(Conv2D(36,5,5,subsample=(2,2),activation="relu"))
-#model.add(Conv2D(48,5,5,subsample=(2,2),activation="relu"))
-#model.add(Conv2D(64,3,3,activation="relu"))
-#model.add(Conv2D(64,3,3,activation="relu"))
-#model.add(Flatten())
-#model.add(Dense(100))
-#model.add(Dense(50))
-#model.add(Dense(10))
 model.compile(loss = 'mse', optimizer = 'adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle = True, nb_epoch=5)
 
 model.save('model.h5')
 
-from keras.models import Model
-import matplotlib.pyplot as plt
+
 
 history_object = model.fit_generator(train_generator, samples_per_epoch =
     len(train_samples), validation_data = 
